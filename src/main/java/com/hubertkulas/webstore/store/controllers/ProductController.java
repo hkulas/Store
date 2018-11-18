d8df53a11c7e52be04dbe6130dfb048ad61fb5ea
Hubert Kulas <hubertzkulas@gmail.com>
2018-11-18 22:51:50 +0100

    Update
    Created ProductController

--- src/main/java/com/hubertkulas/webstore/store/controllers/ProductController.java
+++ src/main/java/com/hubertkulas/webstore/store/controllers/ProductController.java
@@ -1,4 +1,34 @@
 package com.hubertkulas.webstore.store.controllers;
 
+
+import com.hubertkulas.webstore.store.archetype.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/ver01/product")
 public class ProductController {
+
+    @GetMapping
+    public List<Product> list() {
+
+        List<Product> products = new ArrayList<>();
+
+        return products;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@RequestBody Product product){
+
+    }
+
+    @GetMapping("/{id}")
+    public Product get(@PathVariable("id") long id){
+        return new Product();
+    }
+
 }
